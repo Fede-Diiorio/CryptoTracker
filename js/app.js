f8e8cef19bc20a1950ed4ce1503978fc85b96d23
Federico Di Iorio <federico_metallica@hotmail.com>
2023-11-27 16:18:49 -0300

    Se creó una función para obtener y filtrar el valor de las criptos.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 // Funciones
 function obtenerPreciosDeApi() {
-    fetch("http://api.binance.com/api/v3/ticker/price").then((response) => {
-        return response.json();
-    }).then((responseJson) => {
-        console.log(...responseJson);
+    return new Promise((resolve, reject) => {
+        fetch("https://api.binance.com/api/v3/ticker/price")
+            .then((response) => response.json())
+            .then((responseJson) => {
+                for (const cripto of responseJson) {
+                    const filtro = cripto.symbol.includes("USDT");
+                    if (filtro) {
+                        listaDeCriptos.push(cripto);
+                    }
+                }
+                resolve(listaDeCriptos);
+            })
+            .catch((error) => reject(error));
     });
 }
 
-obtenerPreciosDeApi();
+function renderizarListaDeCriptos() {
+    const contenedor = document.getElementById("listaCriptos");
+}
+
+// Variables
+const listaDeCriptos = [];
+
+// Inicio del Programa
+obtenerPreciosDeApi().then(() => {
+    renderizarListaDeCriptos();
+});
+console.log(listaDeCriptos);
